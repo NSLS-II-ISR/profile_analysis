f768e3aeaea0458a01b5658eb30ee4630712f355
Maksim Rakitin <mrakitin@bnl.gov>
2019-08-02 14:41:05 -0400

    Use isort to sort the imports + some little formatting

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,11 +1,10 @@
-from databroker import Broker
-from eiger_io.fs_handler_dask import EigerHandlerDask
+import numpy as np
+
 import chxtools
 import eiger_io
-import numpy as np
 import pyCHX
-
+from databroker import Broker
+from eiger_io.fs_handler_dask import EigerHandlerDask
 
 db = Broker.named('isr')
 db.reg.register_handler('AD_EIGER2', EigerHandlerDask, overwrite=True)
-
